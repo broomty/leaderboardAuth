acee200dd6aeae1a71cb986e333a8e164563e7e5
kato emma <emmanuelkato39@gmail.com>
2024-08-30 13:46:13 +0300

    imporved the functionality

--- services/airtable_services.js
+++ services/airtable_services.js
@@ -1,6 +1,6 @@
 // airtableService.js
 
-import axios from "axios";
+import axios from 'axios';
 
 export class AirtableService {
   constructor(apiKey, baseId, tableName) {
@@ -23,34 +23,30 @@ export class AirtableService {
 
       return records;
     } catch (error) {
-      console.error("Error fetching records:", error);
+      console.error('Error fetching records:', error);
       throw error;
     }
   }
 
-  async authenticateUser(email, view) {
+  async authenticateUser  (email,view) {
     const encodedView = encodeURIComponent(view);
     const response = await axios.get(
-      `https://api.airtable.com/v0/${this.baseId}/staff?filterByFormula={Email}='${email}'&view=${encodedView}`,
+      `https://api.airtable.com/v0/${this.baseId}/${this.tableName}?filterByFormula={Email}='${email}'&view=${encodedView}`,
       {
         headers: {
           Authorization: `Bearer ${this.apiKey}`,
         },
       }
     );
-
-    if (
-      !response.data ||
-      !response.data.records ||
-      response.data.records.length === 0
-    ) {
-      throw new Error("User not found");
+  
+    if (!response.data || !response.data.records || response.data.records.length === 0) {
+      throw new Error('User not found');
     }
-
+  
     const user = response.data.records[0];
-
+  
     return user;
-  }
+  };
 
   async fetchRecords(offset) {
     const config = {
